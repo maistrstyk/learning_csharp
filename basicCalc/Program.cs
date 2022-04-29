91c5b2134d771df0dbad35b605578e60e860d430
Michael Hluchník <hluchnikmichael@gmail.com>
2022-04-29 12:14:15 +0200

    Creating basic command calculator. Need to upgrade functions. First_steps well dont look at it xD

---
+++ basicCalc/Program.cs
@@ -0,0 +1,115 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello in my basic calculator!");
+string number;
+float firstNumber;
+float secondNumber;
+string actionChar = null;
+double result = 0.0;
+bool repeet = true;
+bool answer = true;
+bool wrongSign = true;
+
+
+do
+{
+    Console.WriteLine("\nWrite first number.");
+    while (!float.TryParse(Console.ReadLine(), out firstNumber))
+    {
+        Console.WriteLine("Its not a number. Try again.\n");
+    }
+    
+
+    Console.WriteLine("Choose action:\n" +
+        "+ >> suma\n" +
+        "- >> minus\n" +
+        "* >> times\n" +
+        "/ >> divide\n" +
+        "% >> residue after divide\n" +
+        "^ >> power by 2\n");
+
+    while (wrongSign) { 
+        switch (Console.ReadKey().KeyChar.ToString().ToLower())
+        {
+            case "+":
+                Console.WriteLine("\nWrite second number.");
+                while (!float.TryParse(Console.ReadLine(), out secondNumber))
+                {
+                    Console.WriteLine("\nIts not a number. Try again.\n");
+                }
+                result = firstNumber + secondNumber;
+                wrongSign = false;
+                break;
+            case "-":
+                Console.WriteLine("\nWrite second number.");
+                while (!float.TryParse(Console.ReadLine(), out secondNumber))
+                {
+                    Console.WriteLine("\nIts not a number. Try again.\n");
+                }
+                result = firstNumber - secondNumber;
+                wrongSign = false;
+                break;
+            case "*":
+                Console.WriteLine("\nWrite second number.");
+                while (!float.TryParse(Console.ReadLine(), out secondNumber))
+                {
+                    Console.WriteLine("\nIts not a number. Try again.\n");
+                }
+                result = firstNumber * secondNumber;
+                wrongSign = false;
+                break;
+            case "/":
+                Console.WriteLine("\nWrite second number.");
+                while (!float.TryParse(Console.ReadLine(), out secondNumber))
+                {
+                    Console.WriteLine("\nIts not a number. Try again.\n");
+                }
+                result = firstNumber / secondNumber;
+                wrongSign = false;
+            break;
+            case "%":
+                Console.WriteLine("\nWrite second number.");
+                while (!float.TryParse(Console.ReadLine(), out secondNumber))
+                {
+                    Console.WriteLine("\nIts not a number. Try again.\n");
+                }
+                result = firstNumber % secondNumber;
+                wrongSign = false;
+                break;
+            case "^":
+                result = Math.Pow(firstNumber, 2);
+                wrongSign = false;
+                break;
+            default:
+                Console.WriteLine("\nYou write wrong character. Try again.\n");
+                wrongSign = true;
+                break;
+        }
+    }
+
+    Console.WriteLine("Result: " + result);
+
+
+
+    while (answer)
+    {
+        Console.WriteLine("Do you want calculate again? Write y/n:\n");
+        switch (Console.ReadKey().KeyChar.ToString().ToLower())
+        {
+            case "y":
+                repeet = true;
+                answer = false;
+                break;
+            case "n":
+                repeet = false;
+                answer = false;
+                break;
+            default:
+                Console.WriteLine("\nWrong answer. Write only y/n\n");
+                answer = true;
+                break;
+        }
+
+
+    } ;
+
+} while (repeet);
---
+++ first_steps/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string a,b;
+int cislo;
+/*Console.WriteLine("Napis neco:");
+a = Console.ReadLine();
+
+Console.WriteLine(a + " napis cislo:");
+
+b = Console.ReadLine();
+cislo = int.Parse(b);
+cislo= cislo * 2;
+Console.WriteLine(cislo);
+Console.WriteLine("---------------------------");
+Console.WriteLine("Napis jmeno:");
+a = Console.ReadLine();
+Console.WriteLine("Napis vlastnost:");
+b = Console.ReadLine();
+Console.WriteLine(a + " " + b);
+Console.WriteLine("---------------------------\n" +
+    "Napis cislo:");
+
+cislo = int.Parse(Console.ReadLine());
+Console.WriteLine("Druha mocnina:" + (cislo * cislo));
+Console.WriteLine("Napis polomer kruhu");
+cislo = int.Parse(Console.ReadLine());
+float obvod = 2 * cislo * 3.1415F;
+float obsah = cislo * cislo * 3.1415F;
+Console.WriteLine("Obvod kruhu je: " + obvod + 
+  "\nObsah kruhu je: " + obsah);*/
+
+Console.WriteLine("Zadejte první číslo:");
+float c;
+while (!float.TryParse(Console.ReadLine(), out c))
+    Console.WriteLine("Neplatné číslo, zadejte prosím znovu:");
+
+
+
+
+
